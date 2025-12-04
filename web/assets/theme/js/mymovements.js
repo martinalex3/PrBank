fdc45bca70ba5f816e6fc5a5cc72e6f063e016a4
alex <alex@2DAW-004>
2025-12-04 10:26:15 +0100

    Commit Uploadeado inicial de cara al comienzo del nuevo trabajo

---
+++ web/assets/theme/js/mymovements.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// Cosas a definir para la creacion del js del movimiento
+/*Necesitamos tablas , formularios, enlaces.
+ * SIGNIN: Neceistaremos mejorar en orden:
+ * ((.window, .document)no hacen falta inicializarlos), (inputEmail, inputPassword), buttonSend, formSignIn
+ * A la hora de hacer el functionhandleonload, para guardar la informacion, es util para obtener la informacion del 
+ * sessionStorage, ya que con ello podemos nombrar el correo del proceso del signup para que a la hora de iniciar sesion, se rellene
+ * automaticamente el campo del correo electronico y que el foco este situado en el campo de la contraseña.
+ * seria con functionhandleLoad(){     }
+ * objetos que voy a manejar: document.addEventListener ("load", 
+ */
+
