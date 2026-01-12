ade47632571f2992e96531dcb71a69cc1277a438
alex <alex@2DAW-004>
2026-01-12 14:12:52 +0100

    Commit realizado para indicaciones de delete y create de movimientos Importante modificar html y css

--- web/assets/theme/js/mymovements.js
+++ web/assets/theme/js/mymovements.js
@@ -3,12 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+const regexBasica = /^(\d{2})\/(\d{2})\/(\d{4})$/;
+
+//LEER TABLAS EN EL SERVIDOR (cRud)
+//PATH PARAM DEL SERVIDOR
 const SERVICE_URL = "/CRUDBankServerSide/webresources/movement/account/";
 /**LINEA PROVISIONAL**/
 sessionStorage.setItem("account.id", 2654785441);
-//Linea DOM
+//LINEA DOM
 document.addEventListener("DOMContentLoaded", buildMovementsTable);
-/*Fetch Users in JSON format*/
+/*FETCH MOVEMENTS IN JSON FORMAT*/
 async function fetchMovements() {
     const response = await fetch(SERVICE_URL + `${sessionStorage.getItem("account.id")}`, {
         method: "GET",
@@ -19,12 +23,12 @@ async function fetchMovements() {
     const jsonText = await response.json();
     return jsonText;
 }
-// Generador de funciones que recoge lineas de tabla
+// GENERADOR DE FUNCIONES QUE RECOGEN LA INFORMACION PARA LA TABLA
 function* userRowGenerator(movements) {
     for (const movement of movements) {
         const tr = document.createElement("tr");
         
-        ["id", "timestamp", "amount", "balance", "description"].forEach(field => {
+        ["timestamp", "description", "amount", "balance"].forEach(field => {
             const td = document.createElement("td");
             td.textContent = movement[field];
             tr.appendChild(td);
@@ -33,7 +37,7 @@ function* userRowGenerator(movements) {
         yield tr;
     }
 }
-
+//FUNCION DE CREAR TABLA DE MOVIMIENTOS
 async function buildMovementsTable (){
     const movements = await fetchMovements();
     const tbody = document.querySelector("#tableBody");
@@ -42,3 +46,18 @@ async function buildMovementsTable (){
         tbody.appendChild(row);
     }
 }
+//CREACIÓN DE MOVIMIENTOS (Crud)
+/*Para crear un movimiento deberemos añadir un boton (ya puesto, en el que nos permita añadir movimientos de las cuentas, con una cuantia, en el que se
+ * aumentará o se reducirá el balance dependiendo de si es un ingreso o una retirada, en la parte del html deberá aparecer una pantalla mostrando la
+ * opcion de ingresar o retirar para despues sumar o restar el saldo que haya en dicha cuenta*/
+function createMovement() {
+    
+}
+//ELIMINACION DE MOVIMIENTOS (cruD)
+/*Para la eliminacion de los datos, es muy importante crear un boton fuera de la tabla, para que sea mas facil para el usuario, y solo se podrá
+ * borrar/deshacer el ultimo movimiento. Para ello podemos meter un metodo manejador en el cual debemos  poner el array fuera de la funcion y podremos
+ * eliminarlo con un delete, pero solo el ultimo movimeinto, que se cogera mediante el ID mas largo, o la fecha mas reciente.*/
+function deleteMovement() {
+    
+}
+//ACTUALIZACION DE DATOS EN CUENTAS (crUd)
